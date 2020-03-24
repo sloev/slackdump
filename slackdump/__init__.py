9b7a2d5a84df954ad921d4e9a34f498fab59ea95
sloev <johanned.valbjorn@gmail.com>
2020-03-24 18:50:48 +0100

    lint

--- slackdump/__init__.py
+++ slackdump/__init__.py
@@ -2,9 +2,13 @@ import click
 
 from slackdump.scraper import run_scraper
 
+
 @click.command()
-@click.option("--ROOTURL", "-e", type=str, help="The url to your channel, eg: https://somewhere.slack.com/messages/66666666")
+@click.option(
+    "--ROOTURL",
+    "-e",
+    type=str,
+    help="The url to your channel, eg: https://somewhere.slack.com/messages/66666666",
+)
 def cli(rooturl):
     run_scraper(rooturl)
-
-    
